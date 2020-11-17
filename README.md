83ca746aae1bc0b607a857c0b84337f68b5a331d
marklreyes <mark.l.reyes@gmail.com>
2020-11-17 08:32:04 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# services-grid-reactbootstrap
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/services-grid-reactbootstrap)
---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+export default function App() {
+
+  return (
+  <Container>
+    <h2 class="text-center">Services</h2>
+    <Row>
+      <Col class="text-center">
+        <p>
+          <button type="button" class="btn btn-link" data-toggle="modal" data-target="#fullServicesModalLong">
+            View full list of services I offer and conditions I treat.
+          </button>
+        </p>
+      </Col>
+    </Row>
+    <Row>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+    </Row>
+    <Row>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+    </Row>
+    <Row>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+    </Row>
+    <Row>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+      <Col>1 of 1</Col>
+    </Row>
+  </Container>
+
+
+  );
+}
